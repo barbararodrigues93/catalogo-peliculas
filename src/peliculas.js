53613c676e6248cba25ec886d380cdb6690cd16a
Barbara <barbarargp7@gmail.com>
2026-06-03 18:52:58 +0100

    Módulo peliculas.js - creación de array y funciones

---
+++ src/peliculas.js
@@ -0,0 +1,32 @@
+export const peliculas = [
+  {
+    titulo: "En busca del Arca Perdida",
+    year: 1981,
+    genero: "Aventura",
+    puntuacion: 8.4,
+  },
+  {
+    titulo: "El Imperio Contraataca",
+    year: 1980,
+    genero: "Sci-Fi",
+    puntuacion: 8.7,
+  },
+  { titulo: "Blade Runner", year: 1982, genero: "Sci-Fi", puntuacion: 8.1 },
+  {
+    titulo: "E.T. El Extraterrestre",
+    year: 1982,
+    genero: "Fantasía",
+    puntuacion: 7.9,
+  },
+  {
+    titulo: "Regreso al Futuro",
+    year: 1985,
+    genero: "Comedia",
+    puntuacion: 8.5,
+  },
+  { titulo: "Toro Salvaje", year: 1980, genero: "Drama", puntuacion: 8.1 },
+];
+
+export function buscarPorGenero(genero) {}
+
+export function mejorPuntuada() {}
