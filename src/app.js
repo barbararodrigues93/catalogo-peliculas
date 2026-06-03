baef5949435b45ec1a0b6927cf8cd26e4c892ef7
Barbara <barbarargp7@gmail.com>
2026-06-03 18:55:12 +0100

    Módulo add.js - creación de función ejecutar

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import { peliculas, buscarPorGenero, mejorPuntuada } from "./peliculas.js";
+import { fichaCorta, resumenCatalogo } from "./formato.js";
+
+export default function ejecutar(params) {
+  console.log(resumenCatalogo());
+
+  const mejor = mejorPuntuada();
+  console.log(`Película mejor puntuada: ${fichaCorta(mejor)}`);
+
+  const peliculasGenero = buscarPorGenero("Comedia");
+  console.log(`Películas del género "Comedia": `);
+  peliculasGenero.forEach((pelicula) => {
+    console.log(fichaCorta(pelicula));
+  });
+}
+
+ejecutar();
